b110746ac0b13da4d78dd8c76bc0a4a42545aa3e
Christos Constantinou <snez@snez.org>
2022-10-18 11:08:57 +0300

    Added session ID in the request headers

--- index.js
+++ index.js
@@ -11,26 +11,28 @@ var response = post(mutations.generateCustomerToken);
 var token = response.generateCustomerToken.token;
 
 console.info("Creating an empty guest cart:".cyan);
-response = post(mutations.createGuestCart);
+response = post(mutations.createGuestCart, token);
 var cartId = response.createEmptyCart;
 
 console.info("Adding a simple product to the cart:".cyan);
-response = post(mutations.addSimpleProductToCart.replace("CART_ID", cartId));
+response = post(mutations.addSimpleProductToCart.replace("CART_ID", cartId), token);
 
 console.info("Setting a shipping address:".cyan);
-response = post(mutations.setShippingAddress.replace("CART_ID", cartId));
+response = post(mutations.setShippingAddress.replace("CART_ID", cartId), token);
 
 console.info("Setting a billing address:".cyan);
-response = post(mutations.setBillingAddress.replace("CART_ID", cartId));
+response = post(mutations.setBillingAddress.replace("CART_ID", cartId), token);
 
 console.info("Setting a shipping method:".cyan);
-response = post(mutations.setShippingMethod.replace("CART_ID", cartId));
+response = post(mutations.setShippingMethod.replace("CART_ID", cartId), token);
 
 console.info("Setting a guest email address:".cyan);
-response = post(mutations.setGuestEmail.replace("CART_ID", cartId));
+response = post(mutations.setGuestEmail.replace("CART_ID", cartId), token);
 
 console.info("Setting the payment method:".cyan);
-response = post(mutations.setPaymentMethod.replace("CART_ID", cartId));
+response = post(mutations.setPaymentMethod.replace("CART_ID", cartId), token);
 
 console.info("Placing the order:".cyan);
-response = post(mutations.placeOrder.replace("CART_ID", cartId));
+response = post(mutations.placeOrder.replace("CART_ID", cartId), token);
+
+response = post(mutations.placeOrder.replace("CART_ID", cartId), token);
--- modules/post.js
+++ modules/post.js
@@ -1,16 +1,27 @@
 const fetch = require("sync-fetch");
 const util = require("util");
 
-module.exports = function(query, callback)
+module.exports = function(query, sessionId, callback)
 {
     var response = fetch(process.env.BASE_URL + '/graphql', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
+        'Authorization': 'Bearer ' + sessionId
       },
       body: JSON.stringify({query: query})
-    }).json();
+    });
+
+    try
+    {
+      response = response.json();
+    }
+    catch (e)
+    {
+      console.waring(e.message);
+      return null;
+    }
 
     console.log('Response:', util.inspect(response, false, null, true), "\n");
 
